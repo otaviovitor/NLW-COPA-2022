d8c451ef9b94370bac5caa8ab34eb2866c22f637
Otavio Vitor <otaviodesigner5@gmail.com>
2022-11-01 20:43:44 -0300

    add aula:01 web and mobile

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,13 @@
+import { NativeBaseProvider, Center, Text } from "native-base";
+
+
+
+export function SignIn(){
+  return (
+    <Center flex={1} bgColor="gray.900" >
+        <Text fontSize={24} fontFamily="heading" color="gray.100" >
+            SignIn
+        </Text>
+  </Center>
+  )
+}
